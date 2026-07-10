dcc09c1c55d1f56102342a1f68f909396a058a3e
SilvioVaz <silviodelaravaz@gmail.com>
2026-07-10 11:25:32 -0300

    Delete Alertas_Email_Windows directory

--- Alertas_Email_Windows/Executar_Teste_Agora.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Enviar_Alertas.ps1"
-if errorlevel 1 pause
--- Alertas_Email_Windows/Instalar_Agendamento.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Instalar_Agendamento.ps1"
